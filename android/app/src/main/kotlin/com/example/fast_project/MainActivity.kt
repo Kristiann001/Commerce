78cf0ee8d4acb7362120372fdcb10ad80a2d5e3c
Kristian Koome <originalkristiann@gmail.com>
2026-01-10 13:42:04 +0300

    first commit

---
+++ android/app/src/main/kotlin/com/example/fast_project/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.fast_project
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            const Text(
+              'My Shop',
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+              },
+              child: const Text('Buy Now'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
